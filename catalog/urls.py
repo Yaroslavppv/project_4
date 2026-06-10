e1b6a49665937042adadf2bfd42c3e9561e2c2c5
Ярослав Попов <popow.yaroslaw2012@yandex.ru>
2026-06-10 19:47:06 +0500

    Проделанная работа Урок Шаблонизация

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
     path('', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts')
+    path('contacts/', views.contacts, name='contacts'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail')
 ]
